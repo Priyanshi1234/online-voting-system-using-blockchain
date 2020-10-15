994b614dd64863d5f96c858e0c6f903051955abf
Priyanshi1234 <64778834+Priyanshi1234@users.noreply.github.com>
2020-10-15 17:43:22 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2020 <a href="http://india.govt.in">Satyamev Jayate</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
